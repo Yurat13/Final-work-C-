441f9dead4bbd6196d5ba541f2eaa241e6877b1d
Yurat <yurat.bekmuhamedov@yandex.com>
2022-10-04 01:07:32 +0500

    Final work is done

---
+++ Massiv/Program.cs
@@ -0,0 +1,12 @@
+﻿
+void FindWords (string[] words, int length)
+{
+    
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (words[i].Length <= length) Console.Write ($"{words[i]} ");
+    }
+}
+
+string[] array = {"priv", "kak", "de"};
+FindWords(array, 3);
